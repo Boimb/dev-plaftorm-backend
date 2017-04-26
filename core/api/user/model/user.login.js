b159d84529b9d441a06a2df1b475870d661c59d2
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-04-26 15:32:30 +0200

    remove useless rows in user.login

--- core/api/user/model/user.login.js
+++ core/api/user/model/user.login.js
@@ -1,16 +1,12 @@
 module.exports = login;
 
-const schema = require('../const/user.schema.js');
-const Joi = require('joi');
 const Promise = require('bluebird');
-const validate = Promise.promisify(Joi.validate);
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const uuid = require('uuid');
 const db = require(__base + 'core/service/db.js');
 const config = require(__base + 'config.js');
 const request = require('../const/request.js');
-const saltRounds = 10;
 
 function login(params) {
     
