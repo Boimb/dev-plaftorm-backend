b86732221ee30c95ab0ec5ab5870d5f9185b4174
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-04-26 18:45:59 +0200

    increase test timeout for travis

--- test/core/service/s3.js
+++ test/core/service/s3.js
@@ -1,7 +1,7 @@
 describe('s3', function () {
     describe('.createSignedUrl()', function () {
         it('should return a signed url', function () {
-            this.timeout(3000);
+            this.timeout(6000);
             const s3 = require(__base + 'core/service/s3.js');
             
             return s3.createSignedUrl('dossier', 'file.txt');
