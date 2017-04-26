093e347e8c405130be449a5f5db8f61991d9edbb
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-04-26 11:24:54 +0200

    add config.js, add routes.js

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    db: {
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        max: parseInt(process.env.DB_MAX_CLIENT), // max number of clients in the pool
+        idleTimeoutMillis: parseInt(process.env.DB_IDLE_TIMEOUT), // how long a client is allowed to remain idle before being closed
+    },
+    server: {
+        port: parseInt(process.env.SERVER_PORT)
+    }
+};
