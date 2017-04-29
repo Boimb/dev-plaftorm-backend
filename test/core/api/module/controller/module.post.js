07b2091c681000826e0032080437042135a79820
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-04-29 22:25:56 +0200

    unit test POST /module

---
+++ test/core/api/module/controller/module.post.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const app = require(__base + 'app.js');
+
+describe('module', function () {
+    describe('POST /module', function () {
+        it('should create a module', function () {
+
+            var params = {
+                name: 'test',
+                img: 'http://test.fr',
+                url: 'https://github.com/gladys-test',
+                slug: 'test',
+                min_gladys_version: 3,
+                max_gladys_version: 3
+            };
+
+            return request(app)
+                .post('/module')
+                .set('Authorization', 'JWT ' + process.env.TEST_JWT)
+                .send(params)
+                .expect(201)
+                .then((response) => {
+                    response.body.should.have.property('name');
+                });
+        });
+    });
+});
