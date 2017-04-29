7a1a4652dbdba82b866e5cd26400c69af692cee3
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-04-29 22:22:32 +0200

    user name is required

--- core/api/user/const/user.schema.js
+++ core/api/user/const/user.schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 var schema = Joi.object().keys({
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
-    name: Joi.string().token().min(2)
+    name: Joi.string().token().min(2).required()
 });
 
 module.exports = schema;
