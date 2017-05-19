b8f9d1e42c8542fb545255f5083fbf70c92f4039
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-05-19 16:56:42 +0200

    typo error in module.get

--- core/api/module/controller/module.get.js
+++ core/api/module/controller/module.get.js
@@ -1,7 +1,7 @@
 const Module = require('../model/module.js');
 
 module.exports = function(req, res, next) {
-    Module.get(req.query.lange, req.query.take, req.query.skip)
+    Module.get(req.query.lang, req.query.take, req.query.skip)
         .then((modules) => res.json(modules))
         .catch(next);
 };
