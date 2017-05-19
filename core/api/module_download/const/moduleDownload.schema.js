7a77b09d124e6518cecf78aadd15f2389410f9f2
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-05-19 15:09:08 +0200

    add limit in source size

--- core/api/module_download/const/moduleDownload.schema.js
+++ core/api/module_download/const/moduleDownload.schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 var schema = Joi.object().keys({
     module_version_id: Joi.string().uuid().required(),
     instance_id: Joi.string().uuid().required(),
-    source: Joi.string().required()
+    source: Joi.string().required().max(200)
 });
 
 module.exports = schema;
