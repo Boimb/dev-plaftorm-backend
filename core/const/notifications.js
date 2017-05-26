46a8dbca041dc69c07bd1567aec5d0fa3faf84a7
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-05-26 14:19:10 +0200

    add notifications translations

---
+++ core/const/notifications.js
@@ -0,0 +1,38 @@
+module.exports = {
+    MODULE_IN_REVIEW: {
+        en: 'Your module ${module.name} is waiting for review!',
+        fr: 'Votre module ${module.name} est en attente de review !'
+    },
+    MODULE_ACCEPTED: {
+        en: 'Your module ${module.name} is accepted and now online!',
+        fr: 'Votre module ${module.name} a été accepté, et est maintenant en ligne!'
+    },
+    MODULE_TEXT_IN_REVIEW: {
+        en: 'Texts for your module ${module.name} are waiting for review.',
+        fr: 'Les nouveaux textes pour votre module ${module.name} sont en attentes de review !'
+    },
+    MODULE_TEXT_ACCEPTED: {
+        en: 'Texts for your module ${module.name} has been accepted!',
+        fr: 'Les nouveaux textes pour votre module ${module.name} ont été acceptés !'
+    },
+    SCRIPT_IN_REVIEW: {
+        en: 'Your script ${script.name} is currently waiting for review!',
+        fr: 'Votre script ${script.name} est en attente de review !'
+    },
+    SCRIPT_ACCEPTED: {
+        en: 'Your script ${script.name} is accepted!',
+        fr: 'Votre script ${script.name} a été accepté !'
+    },
+    NEW_MODULE_TO_REVIEW: {
+        en: 'A new module ("${module.name}") has been published and is waiting for review.',
+        fr: 'Un nouveau module  ("${module.name}") a été publié et est en attente de review.'
+    },
+    NEW_SCRIPT_TO_REVIEW: {
+        en: 'A new script ("${script.name}") has been published and is waiting for review.',
+        fr: 'Un nouveau script  ("${script.name}") a été publié et est en attente de review.'
+    },
+    MODULE_REACHED_X_DOWNLOAD: {
+        en: 'Your module "${module.name}" just reached ${module.downloads} downloads ! Congrats!',
+        fr: 'Votre module "${module.name}" vient d\'atteindre ${module.downloads} téléchargements! Congrats!'
+    }
+};
